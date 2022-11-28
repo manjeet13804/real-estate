cae1cd9b41b4a5d5cd0eb61397a423a0956d9a60
Manjeet Panghal <manjeet13804@gmail..com>
2022-11-28 16:06:48 +0530

    real estate react app creted succesfully

---
+++ real-estate-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is the real estate app
+      </header>
+    </div>
+  );
+}
+
+export default App;
