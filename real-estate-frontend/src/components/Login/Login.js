55f9c90d3ced85c9abaa94d8c7d23d8b634f3134
sayam <sampawar159@gmail.com>
2022-12-03 10:46:18 +0530

    pushing updatedFrontend

--- real-estate-frontend/src/components/Login/Login.js
+++ real-estate-frontend/src/components/Login/Login.js
@@ -8,42 +8,49 @@ import axios from 'axios';
 const Login = () => {
 
     const navigate = useNavigate()
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-    const [data, setData] = useState({
-        email: "",
-        password: "",
-    })
-
-const handleChange = ({currentTarget:input})=>{
-    setData({...data,[input.name]:data.value})
-    
-}
-const handleSubmit = async(e) =>{
-    e.preventDefault();
-
-    try{
-        const url = "http://localhost:5000/api/user/login"
-        const {data:res} = await axios.post(url,data)
-        localStorage.setItem("token",res.data)
-        // windows.location('/')
-        navigate('/dashboard')
-        console.log (res.message)
+    const login = (e) => {
+        e.preventDefault();
+        let DATA = {
+            email: email,
+            password: password
+        }
+        localStorage.setItem('userId', DATA.email)
+        axios.post('http://localhost:5000/api/user/login', DATA)
+            .then(function (response) {
+                console.log(response.data.message);
+                if (response.data.message === "success") {
+                    localStorage.setItem('token', response.data.token)
+                    navigate('/dashboard')
+                }
+                else {
+                    alert(response.data.message)
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+                alert('error')
+            });
     }
-    catch(error){
-        alert("Enter valid crendentials")
-    }
-
-}
-
 
     return <div className="main-box">
         <section className="information">
             <img src={PropertyLogo} alt="LOGO" />
             <p className="login-para">Enter your credentials to access your account</p>
-            <form action="#" onSubmit={handleSubmit} >
-                <input className="input-box" type="email" placeholder="User Id" required name="email" value={data.email} onChange={handleChange} /><br />
-                <input className="input-box" type="password" placeholder="password" required name="password" value={data.password}  onChange={handleChange}/><br />
-                <button className="btn">Login</button>
+            <form method="POST" >
+                <input className="input-box" type="email" placeholder="User Id" required name="email"   
+                 onChange={(e) => {
+                                setEmail(e.target.value)
+                            }} /><br />
+
+                <input className="input-box" type="password" placeholder="password" required name="password"      
+                onChange={(e) => {
+                                setPassword(e.target.value)
+                            }}/>
+                            <br />
+                <button className="btn"  onClick={login}>Login</button>
             </form>
             <Link to="/signup">
                 <div className="signup-link">Sign Up</div>
