ed28e16dfa068ff33f980abd86075c44c7f8f526
lalatendupany <lalatendupany.2000@gmail.com>
2022-12-04 23:12:04 +0530

    file upadte sucessfully

--- real-estate-frontend/src/App.js
+++ real-estate-frontend/src/App.js
@@ -4,16 +4,27 @@ import SignUp from './components/SignUp/SignUp';
 import Dashboard from './components/Dashboard/dashboard';
 import Login from './components/Login/Login';
 import AddProperty from "./components/AddProperty/AddProperty";
+import PrivateRouter from './components/Dashboard/PrivateRouter';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login/>}/>
-          <Route path='/add-property' element={<AddProperty/>}/>
-          <Route path='/signup' element={<SignUp/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}>
-          <Route path='add-property' element={<AddProperty/>}/>
+
+          <Route path='/add-property' element={
+            <PrivateRouter>
+          <AddProperty/>
+          </PrivateRouter>}
+          />
+
+         <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/dashboard' element={
+            <PrivateRouter>
+          <Dashboard/>
+          </PrivateRouter>}
+          >
+          
           </Route>
         </Routes>
       </BrowserRouter>
